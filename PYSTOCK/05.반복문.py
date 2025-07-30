c24964f93a40db082f319919ca7dfafc8e69fa93
LeeSangYong <kecondev@gmail.com>
2025-07-30 14:38:57 +0900

    반복문 예제

---
+++ PYSTOCK/05.반복문.py
@@ -0,0 +1,8 @@
+cart = ["apple", "banana", "cherry"] # 장바구니
+for item in cart:
+    print(item)  # 장바구니에 있는 과일을 출력합니다.
+
+num = 0
+while num < 5:
+    print(num)  # 0부터 4까지의 숫자를 출력합니다.
+    num += 1  # num을 1씩 증가시킵니다.
