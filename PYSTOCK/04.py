e9a84d1e4f9da2767077e98382c916fdc548ce16
LeeSangYong <kecondev@gmail.com>
2025-07-30 14:31:52 +0900

    자료구조 복습

--- PYSTOCK/04.py
+++ PYSTOCK/04.py
@@ -1,1 +1,18 @@
-test
+#list
+list = [1, 2, 3, 4, 5] #순서가 존재하고 값변경이 가능
+
+tuple = (1, 2, 3, 4, 5) #순서가 존재하고 값변경이 불가능
+
+dict = {
+    "name": "leesangyong",
+    "age": 52,
+    "company": "devinside"
+    }       #순서가 존재하지 않고 키와 값의 쌍으로 이루어짐
+
+print(list)
+print(list[0])
+print(list[0:3])  # 슬라이싱
+print(tuple)
+print(tuple[0])
+print(dict)
+print(dict["name"])
